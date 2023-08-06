61a9c93dca4a6254e88a2d6920b97f0bfe1208a1
Marc Lorente <mlorente2005@gmail.com>
2023-08-06 15:42:54 +0200

    Improve component tw-follow-card

--- projects/00-twitter-follow-card/src/App.jsx
+++ projects/00-twitter-follow-card/src/App.jsx
@@ -1,23 +1,47 @@
-import "./App.css";
 import { TwitterFollowCard } from "./TwitterFollowCard";
+import "./App.css";
+
+const users = [
+  {
+    userName: "13_mLorente",
+    name: "Marc Lorente Tur",
+    isFollowing: true,
+  },
+  {
+    userName: "midudev",
+    name: "Miguel Ángel Durán",
+    isFollowing: true,
+  },
+  {
+    userName: "pheralb",
+    name: "Pablo H.",
+    isFollowing: false,
+  },
+  {
+    userName: "TMChein",
+    name: "Tomas",
+    isFollowing: true,
+  },
+];
 
 export function App() {
   const formatUserName = (userName) => `@${userName}`;
 
   return (
     <section className="App">
-      <TwitterFollowCard
-        formatUserName={formatUserName}
-        userName="13_mLorente"
-        name="mLorente_13"
-        initialIsFollowing
-      />
-      <TwitterFollowCard
-        formatUserName={formatUserName}
-        userName="midudev"
-        name="Miguel Ángel Durán"
-        initialIsFollowing={false}
-      />
+      {
+        users.map(user => {
+        const { userName, name, isFollowing } = user;
+        return (
+          <TwitterFollowCard
+            key={userName}
+            userName={userName}
+            name={name}
+            initialIsFollowing={isFollowing}
+            formatUserName={formatUserName}
+          ></TwitterFollowCard>
+        );
+      })}
     </section>
   );
 }
